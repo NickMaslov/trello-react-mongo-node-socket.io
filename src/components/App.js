0296b32199121597b579cc5c5e8e3db7c6f02206
Mykola Maslov <mykolamaslov@mykolas-mbp.home>
2019-08-17 17:44:53 -0400

    redux basic setup; list and card components

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import TrelloList from "./TrelloList";
 
 function App() {
   return (
     <div className="App">
       <h2>Hello User</h2>
+      <TrelloList title="test" />
     </div>
   );
 }
---
+++ src/components/TrelloCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TrelloCard() {
+  return (
+    <div>
+      <h3>Hello Card</h3>
+    </div>
+  );
+}
+
+export default TrelloCard;
